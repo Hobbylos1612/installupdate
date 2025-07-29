0b474e56a0901769a4fb5bc44d4e65efcb0bed55
Hobbylos1612 <125378718+Hobbylos1612@users.noreply.github.com>
2025-07-29 15:17:41 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,3 @@
 import requests
 webhook_url = "https://discord.com/api/webhooks/1350538017751830640/WFLLCXiS97voaDbuHnbVAxdVwCfzU5XkHL7_6aVJdlccMijBte8gAC5oiA4aX6MTxXbM"
-requests.post(webhook_url, json={"content": "Hello from Python!"})
+requests.post(webhook_url, json={"content": "Test"})
